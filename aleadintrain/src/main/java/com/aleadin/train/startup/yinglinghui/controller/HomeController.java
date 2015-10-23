9e0e21da80d7285d28d7d6b72ac35c618a91688f
dennistan <totxian@gmail.com>
2015-10-23 17:24:21 +0800

    修改提交

--- aleadintrain/src/main/java/com/aleadin/train/startup/yinglinghui/controller/HomeController.java
+++ aleadintrain/src/main/java/com/aleadin/train/startup/yinglinghui/controller/HomeController.java
@@ -12,13 +12,81 @@ public class HomeController {
 		return "yinlinghuimain";
 	}
 	
-	@RequestMapping(value="/index", method=RequestMethod.GET)
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String index() {
-		return "index";
+		return "yinlinghuimain";
 	}
+	
 	@RequestMapping(value="/test")
 	public @ResponseBody String test()
 	{
-		return "eeeeeeee";
+		return "test success";
+	}
+	
+	//
+	@RequestMapping(value="/ylh/superstarcourse")
+	public String superStarCourse()
+	{
+		return "superstarcourse";
+	}
+	
+	@RequestMapping(value="/ylh/elitecourse")
+	public String eliteCourse()
+	{
+		return "elitecourse";
+	}
+	
+	//
+	@RequestMapping(value="/ylh/topicarticle")
+	public String topicArticle()
+	{
+		return "topicarticle";
+	}
+	
+	@RequestMapping(value="/ylh/datebar")
+	public String dateBar()
+	{
+		return "datebar";
+	}
+	@RequestMapping(value="/careerpreview/onlinecourse")
+	public String onlineCourse()
+	{
+		return "careerpreviewonlinecourse";
+	}
+	
+	@RequestMapping(value="/careerpreview/offlinecourse")
+	public String offlineCourse()
+	{
+		return "careerpreviewofflinecourse";
+	}
+	
+	@RequestMapping(value="/my/accountmgr")
+	public String accountMgr()
+	{
+		return "accountmgr";
+	}
+	
+	@RequestMapping(value="/my/ijionyou")
+	public String iJionYou()
+	{
+		return "ijionyou";
+	}
+	
+	@RequestMapping(value="/my/becomeelite")
+	public String becomeElite()
+	{
+		return "becomeelite";
+	}
+	
+	@RequestMapping(value="/my/eliteclub")
+	public String eliteClub()
+	{
+		return "eliteclub";
+	}
+	
+	@RequestMapping(value="/my/myorder")
+	public String myOrder()
+	{
+		return "myorder";
 	}
 }
